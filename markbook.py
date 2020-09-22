cfd0de852387ae6d068724f48cc5828e505e2673
JinhaSchool <71281106+JinhaSchool@users.noreply.github.com>
2020-09-22 18:42:08 -0400

    Added stuff
    Print report, class average, assignment list, edit_student, student_list, order marks, remove assignment, add assignment, create student

--- markbook.py
+++ markbook.py
@@ -1,72 +1,101 @@
+from typing import Dict, List
 """
 Markbook Application
 Group members: 
 """
-from typing import Dict
 
 
 def create_assignment(name: str, due: str, points: int) -> Dict:
-    """Creates an assignment represented as a dictionary
-    
-    Args:
-        name: the name of the assignment.
-        due: the due date for the assignment.
-        points: what the assignment is out of (denominator).
-    Returns:
-        Assignment as a dictionary.
-    """
     return {"name": name, "due": due, "points": points}
+
+def create_student(first_name: str, last_name: str, gender: str, image, student_number: int, grade: int, email: str,
+                   marks: List[float], comments: str):
+    return {
+        "first_name": first_name,
+        "last_name": last_name,
+        "gender": gender,
+        "image": image,
+        "student_number": student_number,
+        "grade": grade,
+        "email": email,
+        "marks": marks,
+        "comments": comments
+
+    }
+
+def add_assignment(assignment: Dict, classroom: Dict):
+    classroom["assignment_list"].append(assignment)
+    return None
+
+
+def remove_assignment(assignment: Dict, classroom: Dict):
+    classroom["assignment_list"].remove(assignment)
+    return None
 
 
 def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
-    """Creates a classroom dictionary"""
-    return {"course_code": course_code, "course_name": course_name, "period": period, "teacher": teacher, "student_list": [], "assignment_list": []}
+    return {
+        "course_code": course_code,
+        "course_name": course_name,
+        "period": period,
+        "teacher": teacher,
+        "student_list": [],
+        "assignment_list": []
+    }
 
 
 def calculate_average_mark(student: Dict) -> float:
-    """Calculates the average mark of a student"""
-    listy = student["marks"]
+    marks = student["marks"]
     average = 0
-    for i in listy:
-      average += i
+    for i in marks:
+        average += i
 
-    average = average/len(listy)
-    
+    average = average / len(marks)
 
     return average
 
 
 def add_student_to_classroom(student: Dict, classroom: Dict):
-    """Adds student to a classroom
-
-    Args:
-        student: Student dict
-        classroom: The classroom to add the student to
-    """
     classroom["student_list"].append(student)
     return None
-    
 
 
 def remove_student_from_classroom(student: Dict, classroom: Dict):
-    """Removes student from classroom
-
-    Args:
-        student: The student to be removed
-        classroom: the class from which the student will be removed.
-    """
     classroom["student_list"].remove(student)
     return None
 
+def order_marks(student: Dict):
+    marks = student["marks"]
+    return sorted(marks)
 
 
 def edit_student(student: Dict, **kwargs: Dict):
-    """Edits the student's info with the provided key/value pairs
+    for i in kwargs.items():
+        student[i[0]] = i[1]
 
-    Args:
-        student: The student whose data needs to be udated.
-        **kwargs: KeyWordARGumentS. The key/value pairs of the
-            data that needs to be changed. Can come in the form
-            of a dictionary.
-    """
-    pass
+    return None
+
+
+def student_list(classroom: Dict) -> List:
+    return classroom["student_list"]
+
+
+def assignment_list(classroom: Dict) -> List:
+    return classroom["assignment_list"]
+
+def class_average(classroom: Dict) -> float:
+    students = classroom["student_list"]
+    class_avg = 0
+    for i in students:
+        class_avg += calculate_average_mark(i)
+
+    class_avg = class_avg/len(students)
+    return class_avg
+
+
+def print_report(classroom: Dict):
+    print("======================")
+    print("        Report        ")
+    print("Class: " + str(classroom["course_code"]))
+    print("Class Average: " + str(class_average(classroom)))
+    return None
