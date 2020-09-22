c2f25b42e7bfc6c8d95067a568b7b32a9fc8f800
Cars10K <62114960+Cars10K@users.noreply.github.com>
2020-09-22 15:24:35 -0400

    Update markbook.py

--- markbook.py
+++ markbook.py
@@ -55,7 +55,9 @@ def remove_student_from_classroom(student: Dict, classroom: Dict):
         student: The student to be removed
         classroom: the class from which the student will be removed.
     """
-    pass
+    classroom["student_list"].remove(student)
+    return None
+
 
 
 def edit_student(student: Dict, **kwargs: Dict):
