da78458cb8e083031fd3b5d57f8dbb8563dc211e
mark-code <54897881+mark-code@users.noreply.github.com>
2020-09-23 13:22:13 -0400

    Update markbook.py

--- markbook.py
+++ markbook.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+import json
 """
 Markbook Application
 Group members: 
@@ -99,3 +100,9 @@ def print_report(classroom: Dict):
     print("Class: " + str(classroom["course_code"]))
     print("Class Average: " + str(class_average(classroom)))
     return None
+
+def store_student_information(classroom: dict):
+    with open("class_data.json", "w") as write_file:
+        json.dump(classroom, write_file)
+    return None
+    pass
